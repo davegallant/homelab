f263769e377572e12ea069a20112a4b3579ff7ab
Dave Gallant <davegallant@proton.me>
2026-03-27 17:36:17 -0400

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Homelab managed with Ansible";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              ansible
+              just
+            ];
+          };
+        }
+      );
+    };
+}
